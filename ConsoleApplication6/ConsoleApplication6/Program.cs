2c2ee5888fe7f797b69ad0d8bc59a1561c82bb42
Lindsay Strydom <lindsay.strydom@asos.com>
2017-02-02 14:13:35 +0000

    practice commit to git

---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day is it today?");
+            string dayoftheweek = Console.ReadLine();
+
+
+            
+                switch (dayoftheweek.ToUpper())  //makes input case insensitive
+                        
+                {
+                    case "FRIDAY":
+                        Console.WriteLine("FRIYAY"); 
+                        break;
+                    case "SATURDAY":
+                    case "SUNDAY":
+                       Console.WriteLine("IT'S THE FREAKING WEEKEND");
+                        break;
+                    default:
+                        Console.WriteLine("LAME");
+                        break;
+        }
+
+            Console.ReadKey();
+        }
+    }
+}
